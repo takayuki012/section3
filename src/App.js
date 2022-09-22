10ec25e791c5b6325a802bcb66f6f3e22eaf33ec
takayuki012 <113153021+takayuki012@users.noreply.github.com>
2022-09-22 09:34:42 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { useState, useCallback } from "react";
+import { ChildArea } from "./ChildArea";
+import "./styles.css";
+
+export default function App() {
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const onChangeText = (e) => setText(e.target.value);
+  const onClickOpen = () => setOpen(!open);
+  const onClickclose = useCallback(() => setOpen(false), []);
+  return (
+    <div className="App">
+      <input value={text} onChange={onChangeText}></input>
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} onClickclose={onClickclose} />
+    </div>
+  );
+}
